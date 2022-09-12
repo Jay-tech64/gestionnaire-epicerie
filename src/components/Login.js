2caacbf9da8802db07648ed97ec4b06896493815
Jérémy Mailhot <jaytech64@pm.me>
2022-09-12 11:41:15 -0400

    Ajout d'un user à la BD

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,49 @@
-import "./Login.css";
+import { useState } from "react";
+import "../index.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const loginInfo = { username, password };
+
+    setIsLoading(true);
+
+    fetch("http://localhost:8000/users/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginInfo), // Le id est ajouté automatiquement par JSON Server
+    }).then(() => {
+      setIsLoading(false);
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Gestionnaire d'épicerie</h1>
+
       <label htmlFor="username">Identifiant</label>
-      <input type="text" id="username" />
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={({ target }) => setUsername(target.value)}
+        required
+      />
+
       <label htmlFor="password">Mot de passe</label>
-      <input id="password" type="text" />
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+        required
+      />
+
+      {isLoading ? <button>Loading...</button> : <button>Se connecter</button>}
     </form>
   );
 };
