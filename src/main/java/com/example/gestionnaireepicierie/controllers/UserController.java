25413f260c9ce51f222ec9e8602df91078379642
Jérémy Mailhot <jaytech64@pm.me>
2022-09-18 15:49:51 -0400

    Update UserController

--- src/main/java/com/example/gestionnaireepicierie/controllers/UserController.java
+++ src/main/java/com/example/gestionnaireepicierie/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.example.gestionnaireepicierie.controllers;
 
+import com.example.gestionnaireepicierie.repositories.UserRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(final UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 }
