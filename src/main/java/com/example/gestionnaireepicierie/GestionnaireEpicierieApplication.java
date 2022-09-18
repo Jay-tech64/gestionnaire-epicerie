c9b275ee66dfc29a467e14a315b751f0a280391d
Jérémy Mailhot <jaytech64@pm.me>
2022-09-18 15:03:04 -0400

    Ajout de Spring

---
+++ src/main/java/com/example/gestionnaireepicierie/GestionnaireEpicierieApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionnaireepicierie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireEpicierieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireEpicierieApplication.class, args);
+	}
+
+}
