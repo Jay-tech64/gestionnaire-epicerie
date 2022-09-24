3688b22c2d1d840ac52c309cb4435add02438020
Jérémy Mailhot <jaytech64@pm.me>
2022-09-24 11:29:11 -0400

    Modification du Login pour se connecteur au backend

--- src/frontend/src/components/LogIn.js
+++ src/frontend/src/components/LogIn.js
@@ -1,62 +1,67 @@
 const LogIn = (props) => {
- 
-  return (
-    <div className="divStyles d-flex flex-column justify-content-evenly align-items-center">
-      <h1 className="display-1 text-center text-white">{props.title}</h1>
-      <form
-        className="d-flex w-50 h-50 flex-column justify-content-center p-5 bg-primary text-white rounded"
-        onSubmit={props.onSubmit}
-      >
-        <div className="mb-3">
-          <label className="form-label" htmlFor="username">
-            Courriel
-          </label>
-          <input
-            className="form-control"
-            type="email"
-            id="username"
-            value={props.email}
-            onChange={props.changeEmail}
-            required
-          />
-        </div>
+    return (
+        <div className="divStyles d-flex flex-column justify-content-evenly align-items-center">
+            <h1 className="display-1 text-center text-white">{props.title}</h1>
+            <form
+                className="d-flex w-50 h-50 flex-column justify-content-center p-5 bg-primary text-white rounded"
+                onSubmit={props.onSubmit}
+            >
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="username">
+                        Courriel
+                    </label>
+                    <input
+                        className="form-control"
+                        type="email"
+                        id="username"
+                        value={props.email}
+                        onChange={props.changeEmail}
+                        required
+                    />
+                </div>
 
-        <div className="mb-3">
-          <label className="form-label" htmlFor="password">
-            Mot de passe
-          </label>
-          <input
-            className="form-control"
-            id="password"
-            type="password"
-            value={props.password}
-            onChange={props.changePassword}
-            required
-          />
-        </div>
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="password">
+                        Mot de passe
+                    </label>
+                    <input
+                        className="form-control"
+                        id="password"
+                        type="password"
+                        value={props.password}
+                        onChange={props.changePassword}
+                        required
+                    />
+                </div>
 
-        <div className="d-flex mb-3">
-          {props.isLoading ? (
-            <button className="btn btn-info" disabled>
-              Loading...
-            </button>
-          ) : (
-            <button type="submit" className="btn btn-info">
-              Se connecter
-            </button>
-          )}
-          <p className="m-2 text-danger fw-bold">{props.errorMessage}</p>
-        </div>
+                <div className="d-flex mb-3">
+                    {props.isLoading ? (
+                        <button className="btn btn-info" disabled>
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">
+                                    Loading...
+                                </span>
+                            </div>
+                        </button>
+                    ) : (
+                        <button type="submit" className="btn btn-info">
+                            Se connecter
+                        </button>
+                    )}
+                    <p className="m-2 text-danger fw-bold">
+                        {props.errorMessage}
+                    </p>
+                </div>
 
-        <div className="my-3">
-          <p>Vous n'avez pas de compte?</p>
-          <button className="btn btn-info" onClick={props.changeForm}>
-            S'inscrire
-          </button>
+                <div className="my-3">
+                    <p>Vous n'avez pas de compte?</p>
+                    <button className="btn btn-info" onClick={props.changeForm}>
+                        S'inscrire
+                    </button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default LogIn;
--- src/frontend/src/containers/LoginContainer.js
+++ src/frontend/src/containers/LoginContainer.js
@@ -1,58 +1,59 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
+import axios from "../api/axios";
 import LogIn from "../components/LogIn";
 
 const LoginContainer = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const history = useHistory();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    axios
-      .get(`http://localhost:4001/users/${email}`)
-      .then((response) => {
-        setIsLoading(false);
-        if (response.data == null) {
-          setErrorMessage("Utilisateur inexistant");
-        } else if (response.data.password !== password) {
-          setErrorMessage("Courriel ou mot de passe incorrect");
-        } else {
-          history.push("/dashboard", response.data);
-        }
-      })
-      .catch((err) => {
-        console.log(err.response);
-        if (err.name !== "AbortError") {
-          if (err.response.status === 0) {
-            setErrorMessage(
-              "Erreur de connexion avec le serveur. Veuillez réessayer plus tard."
-            );
-          } else {
-            console.log("Erreur:" + err);
-          }
-          setIsLoading(false);
-        }
-      });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const userInfo = { email: email, password: password };
+        setIsLoading(true);
 
-  return (
-    <LogIn
-      title={props.title}
-      changeForm={props.changeForm}
-      onSubmit={handleSubmit}
-      email={email}
-      changeEmail={({ target }) => setEmail(target.value)}
-      password={password}
-      changePassword={({ target }) => setPassword(target.value)}
-      isLoading={isLoading}
-      errorMessage={errorMessage}
-    />
-  );
+        axios
+            .post("sign-in", userInfo)
+            .then((response) => {
+                setIsLoading(false);
+                if (response.data == null) {
+                    setErrorMessage("Utilisateur inexistant");
+                } else {
+                    history.push("/dashboard", response.data);
+                }
+            })
+            .catch((err) => {
+                console.log(err.response);
+
+                if (err.response.status === 0) {
+                    setErrorMessage(
+                        "Erreur de connexion avec le serveur. Veuillez réessayer plus tard."
+                    );
+                } else if (err.response.status === 401) {
+                    setErrorMessage("Courriel ou mot de passe incorrect.");
+                } else {
+                    console.log("Erreur: " + err);
+                }
+                setIsLoading(false);
+            });
+    };
+
+    return (
+        <LogIn
+            title={props.title}
+            changeForm={props.changeForm}
+            onSubmit={handleSubmit}
+            email={email}
+            changeEmail={({ target }) => setEmail(target.value)}
+            password={password}
+            changePassword={({ target }) => setPassword(target.value)}
+            isLoading={isLoading}
+            errorMessage={errorMessage}
+        />
+    );
 };
 
 export default LoginContainer;
