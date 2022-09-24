b43e46822769adbf7f08c6ff421649706c381015
Jérémy Mailhot <jaytech64@pm.me>
2022-09-24 18:15:28 -0400

    Ajout d'articles à la liste nouvelle liste d'épicerie

--- src/frontend/src/components/CreateGrocery.js
+++ src/frontend/src/components/CreateGrocery.js
@@ -1,17 +1,45 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 const CreateGrocery = () => {
+    const [item, setItem] = useState(null);
+    const [articles, setArticles] = useState([
+        "Article 1",
+        "Article 2",
+        "Article 3",
+    ]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setArticles([...articles, item]);
+        setItem("");
+    };
+
     return (
         <div className="divStyles d-flex justify-content-center p-3">
-            <main className="col-sm-10 p-4 bg-white text-center rounded">
-                <h1>Page de la création d'une nouvelle épicerie</h1>
-                <form>
-                    <div className="d-flex">
+            <main className="col-sm-10 p-4 bg-white rounded">
+                <h1 className="text-center">Nouvelle épicerie</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="d-flex my-4">
                         <input
                             type="text"
                             className="form-control"
                             placeholder="Ajouter un élément à votre épicerie"
+                            value={item}
+                            onChange={({ target }) => setItem(target.value)}
                         />
-                        <button className="btn btn-info mx-2">Add</button>
+                        <button className="btn btn-info mx-2">
+                            <FontAwesomeIcon icon={faPlus} />
+                        </button>
                     </div>
+                    <section className="articleList">
+                        <ul>
+                            {articles.map((article, i) => (
+                                <li key={i}>{article}</li>
+                            ))}
+                        </ul>
+                    </section>
                 </form>
             </main>
         </div>
